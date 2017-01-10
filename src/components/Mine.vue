1039ddd8b30eccd9573c89e733cdac855910a1f9
imeng <var.peony@gmail.com>
2017-01-10 14:37:49 +0800

    Router Fix

---
+++ src/components/Mine.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<toolbar></toolbar>
+	    <router-view></router-view>
+    </div>>
+</template>
+
+<script type="text/javascript">
+  import toolbar from './mine/MineToolbar.vue'
+
+  export default {
+    name: 'mine',
+    components:{
+      toolbar
+    }
+  }
+</script>
